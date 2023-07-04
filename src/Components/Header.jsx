9be608d2797eabe265b7a6deb820608fde89319d
ericcnguyen04 <ericc.win@gmail.com>
2023-07-03 22:49:38 -0400

    bathroom

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,12 @@ import '../App.css';
 
 export default function Header() {
     return (
-        <div>
-            ues
+        <div className='header'>
+            <h1>
+                Lifeguard    
+            </h1>
+            <input type='number' placeholder='# of rotation'></input>
+            <input type='number' placeholder='# of stands'></input>
         </div>
     )
 }
