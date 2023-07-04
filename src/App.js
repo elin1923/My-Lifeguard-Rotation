5d77969366effbb01bf56e555146d59506b91f04
ericcnguyen04 <ericc.win@gmail.com>
2023-07-03 22:42:32 -0400

    added whiteboard on app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Header from './Components/Header.jsx';
+import Whiteboard from './Components/Whiteboard.jsx';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Whiteboard />
     </div>
   );
 }
