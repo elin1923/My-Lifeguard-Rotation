1da759e69c0ab52df7dce794d8564cf9e73ceb43
ericcnguyen04 <ericc.win@gmail.com>
2023-07-03 23:16:45 -0400

    functionable header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,9 +6,9 @@ export default function Header() {
             <h1>
                 Lifeguard Rotation  
             </h1>
-            <input type='radio' name='rotation'></input>
-            <input type='radio' name='rotation'></input>
-            <input type='radio' name='rotation'></input>
+            <input type='radio' name='rotation' className='btn btn-scale btn-scale-asc-1'></input>
+            <input type='radio' name='rotation' className='btn btn-scale btn-scale-asc-2' checked></input>
+            <input type='radio' name='rotation' className='btn btn-scale btn-scale-asc-3'></input>
             <input type='number' placeholder='# of stands'></input>
             <input type='number' placeholder='Duration'></input>
         </div>
