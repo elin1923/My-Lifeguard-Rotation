02b090283a5bfc98117bbc8020f76cfb38c98798
ericcnguyen04 <ericc.win@gmail.com>
2023-07-04 22:31:39 -0400

    dived up

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,11 +6,13 @@ export default function Header() {
             <h1>
                 Lifeguard Rotation  
             </h1>
-            <input type='radio' name='rotation' className='btn btn-scale btn-scale-asc-1'></input>
-            <input type='radio' name='rotation' className='btn btn-scale btn-scale-asc-2' checked></input>
-            <input type='radio' name='rotation' className='btn btn-scale btn-scale-asc-3'></input>
-            <input type='number' placeholder='# of stands'></input>
-            <input type='number' placeholder='Duration'></input>
+            <div>
+                <input type='radio' name='rotation' className='btn btn-scale btn-scale-asc-1'></input>
+                <input type='radio' name='rotation' className='btn btn-scale btn-scale-asc-2' checked></input>
+                <input type='radio' name='rotation' className='btn btn-scale btn-scale-asc-3'></input>
+                <input type='number' placeholder='# of stands'></input>
+                <input type='number' placeholder='Duration'></input>
+            </div>
         </div>
     )
 }
