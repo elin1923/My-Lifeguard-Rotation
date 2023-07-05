a269cac73365aff7ad561511e34f43bcc900231d
ericcnguyen04 <ericc.win@gmail.com>
2023-07-04 22:37:24 -0400

    clock displayed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
                 <input type='number' placeholder='Duration'></input>
             </div>
             <div>
-                time
+                {clock}
             </div>
         </div>
     )
