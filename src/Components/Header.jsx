7a5b445227049b23e3ae77f620edc126d0012671
ericcnguyen04 <ericc.win@gmail.com>
2023-07-04 22:45:00 -0400

    imported usestate and useeffect

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,10 @@
 import '../App.css';
+import React, {useState, useEffect} from 'react';
 
 export default function Header() {
 
     const date = new Date()
-    const clock = date.getHours() + ':' + date.getMinutes()
+    const clock = date.getHours() + ':' + date.getMinutes() + ":" + date.getSeconds()
 
     return (
         <div className='header'>
