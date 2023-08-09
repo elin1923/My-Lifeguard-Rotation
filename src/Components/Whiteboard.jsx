cce18f274c018acc2ebb4ea22467e46680beb6c8
ericcnguyen04 <ericc.win@gmail.com>
2023-08-08 22:30:49 -0400

    yes

--- src/Components/Whiteboard.jsx
+++ src/Components/Whiteboard.jsx
@@ -5,6 +5,8 @@ export default function Whiteboard({receivedData}) {
     // define a number of stands
 
     // define a number of guards
+
+    // create div based on the number of guards
     return (
         <div className="whiteboard">
             {receivedData && (
